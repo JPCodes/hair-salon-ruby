f4b9aef9c60f347a9d625425ea03ee2ba7010bda
Jim Padilla <jp_code2013@yahoo.com>
2016-12-09 10:20:20 -0800

    Add save, all, == methods and specs

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -5,5 +5,28 @@ class Stylist
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
+  # Create
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i
+  end
+
+  # Read
+  define_singleton_method(:all) do
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each() do |stylist|
+      name = stylist.fetch('name')
+      id = stylist.fetch('id').to_i
+      stylists.push(Stylist.new({:id => id, :name => name}))
+    end
+    stylists
+  end
+
+  # Double equals modify for RSpec
+  define_method(:==) do |other_stylist|
+    self.name().==(other_stylist.name()).&(self.id().==(other_stylist.id()))
+  end
   
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -9,4 +9,20 @@ describe(Stylist) do
     end
   end
 
+  describe('#save') do
+    it ('saves a stylist to the database') do
+      test_stylist = Stylist.new({:id => nil, :name => 'Jane Doe'})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe('#==') do
+    it ('returns true when comparing two different objects with identical attributes') do
+      test_stylist1 = Stylist.new({:name => 'Jane Doe', :id => 1})
+      test_stylist2 = Stylist.new({:name => 'Jane Doe', :id => 1})
+      expect(test_stylist1).to(eq(test_stylist2))
+    end
+  end
+
 end
