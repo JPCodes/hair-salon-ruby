5834b9a577e041fe971d0b284570e48dcc98a060
Jim Padilla <jp_code2013@yahoo.com>
2016-12-09 09:16:09 -0800

    Add spec/methods for == and save

--- lib/client.rb
+++ lib/client.rb
@@ -1,5 +1,5 @@
 class Client
-  attr_reader(:id, :name, :phone)
+  attr_reader(:name, :phone, :stylist_id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
@@ -8,11 +8,31 @@ class Client
   end
 
   # Create
+  define_method(:save) do
+    DB.exec("INSERT INTO clients (name, phone, stylist_id) VALUES ('#{@name}', '#{@phone}', #{@stylist_id})")
+  end
 
   # Read
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients")
+    all_clients = []
+    returned_clients.each() do |client|
+      name = client.fetch('name')
+      phone = client.fetch('phone')
+      stylist_id = client.fetch('stylist_id').to_i
+      all_clients.push(Client.new({:name => name, :phone => phone, :stylist_id => stylist_id}))
+    end
+    all_clients
+  end
 
   # Update
 
   # Delete
+
+
+  # Double equals modify for RSpec
+  define_method(:==) do |other_client|
+    self.name().==(other_client.name()).&(self.phone().==(other_client.phone())).&(self.stylist_id().==(other_client.stylist_id()))
+  end
 
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -10,4 +10,21 @@ describe(Client) do
     end
   end
 
+  describe('#save') do
+    it ('saves a client to the database') do
+      test_client = Client.new({:name => 'Jane Doe', :phone => '4441235555', :stylist_id => 1})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
+
+  describe('#==') do
+    it ('returns true when comparing two different objects with identical attributes') do
+      test_client1 = Client.new({:name => 'Isaac Newton', :phone => '3235551234', :stylist_id => 2})
+      test_client2 = Client.new({:name => 'Isaac Newton', :phone => '3235551234', :stylist_id => 2})
+      expect(test_client1).to(eq(test_client2))
+    end
+  end
+
+
 end
