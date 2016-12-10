ab66c70656e912f4797bbb13debed9e00b268a7b
Jim Padilla <jp_code2013@yahoo.com>
2016-12-09 16:10:57 -0800

    Add integ spec for creating and viewing a client

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -17,3 +17,17 @@ describe('Add a new stylist and see the stylists information', {:type => :featur
     expect(page).to have_content('Manage Joan of Arc')
   end
 end
+
+describe('Add a new client and see the clients information', {:type => :feature}) do
+  it('allows a user to click a client to see their details') do
+    visit('/')
+    click_link('See list of clients')
+    click_link('Add a new client to the system')
+    fill_in('client_name', :with => 'Marie Curie')
+    fill_in('client_phone', :with => '96')
+    click_button('Add client')
+    click_link('Marie Curie')
+    expect(page).to have_content('Information for Marie Curie')
+    expect(page).to have_content('96')
+  end
+end
