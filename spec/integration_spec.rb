b748de87e31feb0bcd779a054a76a763e82adf82
Jim Padilla <jp_code2013@yahoo.com>
2016-12-09 16:32:21 -0800

    Add integ spec for stylist assignment to client

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -61,3 +61,24 @@ describe('Rename a client', {:type => :feature}) do
     expect(page).to have_content('Information for Ada Lovelace')
   end
 end
+
+describe('Add a new client and see the clients information', {:type => :feature}) do
+  it('allows a user to click a client to see their details') do
+    visit('/')
+    click_link('See list of stylists')
+    click_link('Add a new stylist to the system')
+    fill_in('stylist_name', :with => 'Joan of Arc')
+    click_button('Add stylist')
+    visit('/')
+    click_link('See list of clients')
+    click_link('Add a new client to the system')
+    fill_in('client_name', :with => 'Marie Curie')
+    fill_in('client_phone', :with => '96')
+    click_button('Add client')
+    click_link('Marie Curie')
+    expect(page).to have_content('Information for Marie Curie')
+    click_link('Assign a Stylist')
+    click_button('Assign')
+    expect(page).to have_content('Joan of Arc')
+  end
+end
